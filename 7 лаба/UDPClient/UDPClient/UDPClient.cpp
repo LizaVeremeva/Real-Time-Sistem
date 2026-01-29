b5f55f54b70e61bba2738e0d8eaea6e2784233bb
LizaVeremyeva <VMelizaveta@stud.kantiana.ru>
2026-01-29 15:05:14 +0200

    UDP

---
+++ 7 лаба/UDPClient/UDPClient/UDPClient.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+void main() {
+    // Инициализация WINSOCK
+    WSADATA data;
+    int wsOk = WSAStartup(MAKEWORD(2, 2), &data);
+    if (wsOk != 0) {
+        cout << "Can't start Winsock! " << wsOk;
+        return;
+    }
+
+    // Создание hint сервера
+    sockaddr_in server;
+    server.sin_family = AF_INET;
+    server.sin_port = htons(54000);
+    inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
+
+    // Создание UDP сокета
+    SOCKET out = socket(AF_INET, SOCK_DGRAM, 0);
+
+    string msg = "";
+    while (true) {
+        msg.clear();
+        cout << "> ";
+        cin >> msg; // читаем из консоли (до пробела!)
+
+        if (msg.empty()) break;
+
+        // Отправляем сообщение
+        int sendOk = sendto(out, msg.c_str(), msg.size() + 1, 0,
+            (sockaddr*)&server, sizeof(server));
+        if (sendOk == SOCKET_ERROR) {
+            cout << "That didn't work! " << WSAGetLastError() << endl;
+        }
+    }
+
+    // Закрываем сокет
+    closesocket(out);
+    WSACleanup();
+}
---
+++ 7 лаба/UDPServer/UDPServer/UDPServer.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+void main() {
+    // Инициализация WINSOCK
+    WSADATA data;
+    WORD version = MAKEWORD(2, 2);
+    int wsOk = WSAStartup(version, &data);
+    if (wsOk != 0) {
+        cout << "Can't start Winsock! " << wsOk;
+        return;
+    }
+
+    // Создание UDP сокета
+    SOCKET in = socket(AF_INET, SOCK_DGRAM, 0);
+
+    // Настройка адреса сервера
+    sockaddr_in serverHint;
+    serverHint.sin_addr.S_un.S_addr = ADDR_ANY;
+    serverHint.sin_family = AF_INET;
+    serverHint.sin_port = htons(54000);
+
+    // Привязка сокета
+    if (bind(in, (sockaddr*)&serverHint, sizeof(serverHint)) == SOCKET_ERROR) {
+        cout << "Can't bind socket! " << WSAGetLastError() << endl;
+        return;
+    }
+
+    sockaddr_in client;
+    int clientLength = sizeof(client);
+    char buf[1024];
+
+    // Запускаем жизненный цикл
+    while (true) {
+        ZeroMemory(&client, clientLength);
+        ZeroMemory(buf, 1024);
+
+        // Ждем сообщение
+        int bytesIn = recvfrom(in, buf, 1024, 0, (sockaddr*)&client, &clientLength);
+        if (bytesIn == SOCKET_ERROR) {
+            cout << "Error receiving from client " << WSAGetLastError() << endl;
+            continue;
+        }
+
+        char clientIp[256];
+        ZeroMemory(clientIp, 256);
+
+        // Конвертируем адрес в строку
+        inet_ntop(AF_INET, &client.sin_addr, clientIp, 256);
+        // Показываем сообщение
+        cout << "Message recv from " << clientIp << " : " << buf << endl;
+    }
+
+    // Закрываем socket
+    closesocket(in);
+    // Отключаем winsock
+    WSACleanup();
+}
